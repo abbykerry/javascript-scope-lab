824d7a151735d73e6f495022cd8bda019e73500b
Abigael Ongwae <abbykerry17@gmail.com>
2025-12-02 01:09:20 +0300

    modified index.js file

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+//variable data
+const burgers = ["Hamburger", "Cheeseburger"];
+let featuredDrink = "Strawberry Milkshake";
+
+//function and block
+function addBurger(){
+    let newBurger = "Flatburger";
+    burgers.push(newBurger);
+
+    if(true){
+        let anotherNewBurger = "Maple Bacon Burger";
+        burgers.push(anotherNewBurger);
+    }
+}
+function changeFeaturedDrink(){
+    featuredDrink = "The Java Shake";
+   console.log(featuredDrink);
+}
+changeFeaturedDrink();
+console.log(`updated burgers: ${burgers}`);
+console.log(`updated drinks: ${featuredDrink}`);
+//variable declaration and scope
